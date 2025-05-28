1b71157a7e431e7e420cbe4e15a57c918a41af1d
HenriqueShimada <henrique.shimada@sptech.school>
2025-05-28 13:45:07 -0300

    api de cadastro e login funcional

--- Entre Caçadores, Colossos e Baldurianos/src/controllers/usuarioController.js
+++ Entre Caçadores, Colossos e Baldurianos/src/controllers/usuarioController.js
@@ -19,27 +19,20 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
-
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
+                    if (resultadoAutenticar.length > 0) {
                                     res.json({
                                         id: resultadoAutenticar[0].id,
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
                                     });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
                         res.status(403).send("Mais de um usuário com o mesmo login e senha!");
                     }
                 }
+            }
             ).catch(
                 function (erro) {
                     console.log(erro);
@@ -71,6 +64,7 @@ function cadastrar(req, res) {
         usuarioModel.cadastrar(nome, email, senha)
             .then(
                 function (resultado) {
+                    
                     res.json(resultado);
                 }
             ).catch(
